d06f15be4a4fbff94b8cb687a3110a72f25b16a6
declineaberdeen <declineaberdeen@foxmail.com>
2026-09-10 20:36:01 +0800

    feat: initialize theme wallpaper miniapp

---
+++ server/Wallpaper.Api/Data/WallpaperDbContext.cs
@@ -0,0 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using Wallpaper.Api.Models;
+
+namespace Wallpaper.Api.Data;
+
+public sealed class WallpaperDbContext(DbContextOptions<WallpaperDbContext> options) : DbContext(options)
+{
+    public DbSet<Wallpaper> Wallpapers => Set<Wallpaper>();
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.Entity<Wallpaper>(entity =>
+        {
+            entity.HasIndex(x => x.ExternalId).IsUnique();
+            entity.HasIndex(x => new { x.Category, x.UpdatedAtUtc });
+            entity.Property(x => x.Title).HasMaxLength(300);
+            entity.Property(x => x.SourceName).HasMaxLength(80);
+        });
+    }
+}
